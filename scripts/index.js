7f22658d88e5e01c4ff38aee1e51eeb5042f6d3c
Alexander Chukharev <chooharev@gmail.com>
2024-02-06 08:23:18 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const listOfPlaces = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard(card, removePlace) {
-  const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
   // наполняем содержимым
   cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
   cardElement.querySelector('.card__title').textContent = card.name;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', () => {removePlace(deleteButton)}
-  )
+  deleteButton.addEventListener('click', () => removePlace(cardElement));
 
   // отображаем на странице
-  listOfPlaces.append(cardElement); 
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function removePlace(removeButton) {
-  const listItem = removeButton.closest('.places__item');
-  listItem.remove();
+function removePlace(card) {
+  card.remove();
 }
 
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(card => createCard(card, removePlace));
+initialCards.forEach(item => {
+  const card = createCard(item, removePlace);
+  cardsContainer.prepend(card); 
+});
 
 
